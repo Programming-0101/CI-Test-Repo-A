873de9f5bde65627770d5c9b8802bd708aef682b
dagilleland <dagilleland@shaw.ca>
2018-02-09 19:38:46 -0700

    nuspec file with new Assembly info

--- src/Brain.Interfaces/Properties/AssemblyInfo.cs
+++ src/Brain.Interfaces/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Brain.Interfaces")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("Simple Brain interfaces. A silly little library for playing with CI and CD.")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCompany("Dynamic Generation Inc.")]
 [assembly: AssemblyProduct("Brain.Interfaces")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
